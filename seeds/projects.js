cf32dce79efd375f1e7f57da160355fcae2196b6
james-built <jgbuilt@gmail.com>
2018-10-29 21:08:21 +1300

    added some project seed data

--- seeds/projects.js
+++ seeds/projects.js
@@ -1,13 +1,12 @@
 
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('projects').del()
     .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('projects').insert([
+        {id: 2001, user_id: 1001, name: 'Battle Bot Competition', description: 'Building a robot for an upcoming battlebot competition', image: 'https://media.wired.com/photos/5ae226425581121251d2feb0/master/pass/battlebots-598125354.jpg', my_skills_equipment: 'Soldering, PCB Design', required_skills_equipment: 'Welding, CAD, 3D Scanner'},
+        {id: 2002, user_id: 1002, name: '', description: '', image: '', my_skills_equipment: '', required_skills_equipment: ''},
+        {id: 2003, user_id: 1003, name: '', description: '', image: '', my_skills_equipment: '', required_skills_equipment: ''},
+        {id: 2004, user_id: 1004, name: '', description: '', image: '', my_skills_equipment: '', required_skills_equipment: ''}        
       ]);
     });
 };
