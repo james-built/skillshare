342b7fafb565f303723fe70075bd9bdedc230940
james-built <jgbuilt@gmail.com>
2018-11-24 20:08:50 +1300

    created store for redux, reducers not yet made

--- client/index.js
+++ client/index.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { combineReducers } from 'redux'
 
 import App from './src/components/App'
+import users from './src/reducers/users'
+
+const reducers = combineReducers({
+  users
+})
+const store = createStore(reducers)
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+      <App />
+    </Provider>,
     document.getElementById('app')
   )
 })
